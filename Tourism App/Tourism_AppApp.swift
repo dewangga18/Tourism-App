4f145e763ad589babbba975c24e3434955f9af58
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2024-07-24 21:35:15 +0700

    Initial Commit

---
+++ Tourism App/Tourism_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tourism_AppApp.swift
+//  Tourism App
+//
+//  Created by admin on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Tourism_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tourism AppUITests/Tourism_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tourism_AppUITestsLaunchTests.swift
+//  Tourism AppUITests
+//
+//  Created by admin on 24/07/24.
+//
+
+import XCTest
+
+final class Tourism_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
